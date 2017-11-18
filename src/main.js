72191efc0726407a137e06ea9a008700d98d3657
Brendan Viscomi <github@bmv.email>
2017-11-18 18:00:05 -0500

    Live-updating time and hardcoded background image

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+import moment from 'moment';
+
+function initPage() {
+  updateTime();
+  window.setInterval(updateTime, 1000);
+}
+
+let timeStr = null;
+function updateTime() {
+  const currTime = moment().format('HH:mm');
+  if (currTime !== timeStr) {
+    document.getElementById('time').textContent = currTime;
+    timeStr = currTime;
+  }
+}
+
+window.onload = initPage;
