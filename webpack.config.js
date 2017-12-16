a2e14de648d1c6273cb2ec042b3f168e3253b7ee
Brendan Viscomi <github@bmv.email>
2017-12-16 14:05:50 -0500

    Support different manifest.json for Chrome/Firefox
    Some values in the manifest.json differ between
    Chrome and Firefox. Update the webpack config to
    build separately for both browsers. Currently, the
    builds are the same except for minor differences
    between the manifest.json files. It's a bit overkill
    but this makes it very easy to zip up the extension
    for either browser.

--- webpack.config.js
+++ webpack.config.js
@@ -3,50 +3,94 @@ var path = require('path');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var WebpackExtensionManifestPlugin = require('webpack-extension-manifest-plugin');
 
-module.exports = {
-  context: path.resolve(__dirname, 'src'),
-  entry: {
-    'main': ['babel-polyfill', './js/main.js'],
-    'options': ['babel-polyfill', './js/options.js'],
-  },
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: './js/[name].bundle.js'
-  },
-  plugins: [
-    new CleanWebpackPlugin(['build']),
-    new HtmlWebpackPlugin({
-      template: 'index.html',
-      inject: 'body',
-      chunks: ['main'],
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'options.html',
-      template: 'options.html',
-      inject: 'body',
-      chunks: ['options'],
-    }),
-    new CopyWebpackPlugin([
-      { from: './manifest.json' },
-    ])
-  ],
-	module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env', 'flow', 'react'],
-            plugins: ["transform-object-rest-spread", "transform-class-properties"]
+function buildManifest(browser) {
+  var isChrome = browser == 'chrome';
+  var manifest = {
+    manifest_version: 2,
+    name: "Calm New Tab",
+    version: "0.1",
+    description: "Simple extension to replace your new tab page with a relaxing photo.",
+
+    chrome_url_overrides: {
+      newtab: "index.html"
+    },
+
+    options_ui: {
+      page: "options.html",
+      [isChrome ? 'chrome_style' : 'browser_style']: true
+    },
+
+    permissions: [
+      "storage"
+    ]
+  };
+
+  if (!isChrome) {
+    manifest.applications = {
+      gecko: {
+        id: "calmtab@exampleID"
+      }
+    }
+  }
+
+  return manifest;
+}
+
+function getConfig(browser) {
+  var buildDir = 'build/' + browser;
+  return {
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+      'main': ['babel-polyfill', './js/main.js'],
+      'options': ['babel-polyfill', './js/options.js'],
+    },
+    output: {
+      path: path.resolve(__dirname, buildDir),
+      filename: './js/[name].bundle.js'
+    },
+    plugins: [
+      new CleanWebpackPlugin([buildDir]),
+      new HtmlWebpackPlugin({
+        template: 'index.html',
+        inject: 'body',
+        chunks: ['main'],
+      }),
+      new HtmlWebpackPlugin({
+        filename: 'options.html',
+        template: 'options.html',
+        inject: 'body',
+        chunks: ['options'],
+      }),
+      new WebpackExtensionManifestPlugin({
+        config: {
+          base: buildManifest(browser)
+        }
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['env', 'flow', 'react'],
+              plugins: ["transform-object-rest-spread", "transform-class-properties"]
+            }
           }
         }
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  }
-};
+      ]
+    },
+    resolve: {
+      extensions: ['.js', '.jsx']
+    }
+  };
+}
+
+module.exports = [
+  getConfig('chrome'),
+  getConfig('firefox'),
+];
